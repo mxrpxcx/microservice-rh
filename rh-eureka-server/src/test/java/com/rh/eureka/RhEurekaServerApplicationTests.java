3fadf7687e7106bc77db7cdca8a1c5f7cf30667e
Renan <renanfgfg@gmail.com>
2024-07-07 17:34:18 -0300

    eureka server e ajustes no ms folha

---
+++ rh-eureka-server/src/test/java/com/rh/eureka/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rh-folha/src/main/java/com/rh/folha/service/FuncionarioServiceInstanceListSupplier.java
@@ -0,0 +1,27 @@
+package com.rh.folha.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+
+import reactor.core.publisher.Flux;
+
+public class FuncionarioServiceInstanceListSupplier implements ServiceInstanceListSupplier {
+
+	@Override
+	public String getServiceId() {
+		return "hr-funcionario";
+	}
+
+	@Override
+	public Flux<List<ServiceInstance>> get() {
+		List<ServiceInstance> instances = Arrays.asList(
+				new DefaultServiceInstance(getServiceId() + "1", "hr-funcionario", "localhost", 8081, false),
+				new DefaultServiceInstance(getServiceId() + "2", "hr-funcionario", "localhost", 8082, false));
+		return Flux.just(instances);
+	}
+
+}
