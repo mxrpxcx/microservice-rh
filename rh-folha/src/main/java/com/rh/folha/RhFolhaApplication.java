5bb425f9590496eb3238722ff339dfbb266423f8
Renan <renanfgfg@gmail.com>
2024-07-07 13:17:43 -0300

    proj folha

---
+++ rh-folha/src/main/java/com/rh/folha/RhFolhaApplication.java
@@ -0,0 +1,13 @@
+package com.rh.folha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFolhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhFolhaApplication.class, args);
+	}
+
+}
