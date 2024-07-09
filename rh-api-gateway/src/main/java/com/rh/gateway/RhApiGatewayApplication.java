26eeefe7c9a9f893588edd59190488c6b6305089
Renan Godoy <renan.godoy@e-governe.com>
2024-07-09 13:02:39 -0300

    add api gateway project

---
+++ rh-api-gateway/src/main/java/com/rh/gateway/RhApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApiGatewayApplication.class, args);
+	}
+
+}
